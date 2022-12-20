0e13ae04a207bf0a631f78bb3f0e00890b1f8cba
Parth Gupta <parthgupta3003@gmail.com>
2022-12-20 21:03:42 +0530

    ..

--- mousecontrol.py
+++ mousecontrol.py
@@ -43,6 +43,15 @@ while True:
                 # cv2.circle(img, (lineInfo[4], lineInfo[5]),
                 # 15, (0, 255, 0), cv2.FILLED)
             pyautogui.click()
+        
+        if fingers[1] == 0 and fingers[2] == 1 and fingers[3] == 0:
+            length, img, lineInfo = detector.findDistance(8, 12, img)
+            # print(length)
+            # if length > 40:
+                # cv2.circle(img, (lineInfo[4], lineInfo[5]),
+                # 15, (0, 255, 0), cv2.FILLED)
+            pyautogui.rightClick()
+
         if fingers[1] == 1 and fingers[2] == 1 and fingers[3] == 0:
             x3 = np.interp(x1, (frameR, wCam - frameR), (0, wScr))
             y3 = np.interp(y1, (frameR, hCam - frameR), (0, hScr))
